3eda494bef68f45e1631d98d89e9cd69a0512150
neokeld <arnaud.duforat@protonmail.ch>
2018-12-23 13:44:33 +0100

    Commandes: simple implem of a reactive websocket that send a flux of json events

--- commandes/src/main/java/fr/duforat/demos/commandes/handler/ReactiveWebSocketHandler.java
+++ commandes/src/main/java/fr/duforat/demos/commandes/handler/ReactiveWebSocketHandler.java
@@ -1,12 +1,55 @@
 package fr.duforat.demos.commandes.handler;
 
+import java.time.Duration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.WebSocketSession;
 
-public class ReactiveWebSocketHandler {
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-	public void handle(WebSocketSession session) {
-		// TODO Auto-generated method stub
-		
-	}
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+@Component("ReactiveWebSocketHandler")
+public class ReactiveWebSocketHandler implements WebSocketHandler {
+
+    private static final long INTERVAL = 800L;
+	private static final ObjectMapper json = new ObjectMapper();
+    private Logger logger = LoggerFactory.getLogger(ReactiveWebSocketHandler.class);
+
+    
+    // Generate Flux of Json Events
+    private Flux<String> eventFlux = Flux.generate(sink -> {
+        try {
+        	final String jsonStr = json.writeValueAsString("test value");
+        	logger.info(jsonStr);
+            sink.next(jsonStr);
+        } catch (JsonProcessingException e) {
+            sink.error(e);
+        }
+    });
+
+    // Emits each second a Json Event
+    private Flux<String> intervalFlux = Flux.interval(Duration.ofMillis(INTERVAL))
+      .zipWith(eventFlux, (time, event) -> event);
+
+    // When a new connection is established
+    // Send on web socket
+    // A flux of Json Event converted to TextMessage
+    // And
+    // When a flux of inbound events has been received
+    // convert them to text
+    // and trace them in logs
+    @Override
+    public Mono<Void> handle(WebSocketSession webSocketSession) {
+        return webSocketSession.send(intervalFlux
+          .map(webSocketSession::textMessage))
+          .and(webSocketSession.receive()
+            .map(WebSocketMessage::getPayloadAsText).log());
+    }
 }
