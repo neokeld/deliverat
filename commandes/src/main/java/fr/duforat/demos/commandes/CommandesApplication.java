2a30b5675ea2909b2dc98017cb8acfbbb5a1269f
neokeld <arnaud.duforat@protonmail.ch>
2018-12-23 12:09:25 +0100

    init Webflux websocket netty Commande Service

---
+++ commandes/src/main/java/fr/duforat/demos/commandes/CommandesApplication.java
@@ -0,0 +1,14 @@
+package fr.duforat.demos.commandes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommandesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandesApplication.class, args);
+	}
+
+}
+
---
+++ commandes/src/test/java/fr/duforat/demos/commandes/CommandesApplicationTests.java
@@ -0,0 +1,17 @@
+package fr.duforat.demos.commandes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommandesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
