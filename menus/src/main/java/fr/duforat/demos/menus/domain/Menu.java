06ea184b6be242d7c73b7fe5126f77c28fb7ee08
neokeld <arnaud.duforat@protonmail.ch>
2018-12-27 23:35:50 +0100

    Menus: add picture and popularity

--- menus/src/main/java/fr/duforat/demos/menus/domain/Menu.java
+++ menus/src/main/java/fr/duforat/demos/menus/domain/Menu.java
@@ -9,19 +9,23 @@ public class Menu {
 		private String title;
 		private String description;
 		private BigDecimal price;
+		private String picture;
+		private boolean popular;
 
 		public Menu() {
 			
 		}
 		
-		public Menu(String title, String description, BigDecimal price) {
+		public Menu(String title, String description, BigDecimal price, String picture, boolean popular) {
 			this.title = title;
 			this.description = description;
 			this.price = price;
+			this.picture = picture;
+			this.popular = popular;
 		}
 
 		public static Menu fakeMenuFactory() {
-			return new Menu("test", "test", new BigDecimal("1.00"));
+			return new Menu("test", "test", new BigDecimal("1.00"), "//f.roocdn.com/images/menu_items/1583350/item-image.jpg", true);
 		}
 		
 		public String getTitle() {
@@ -55,5 +59,21 @@ public class Menu {
 
 		public void setPrice(BigDecimal price) {
 			this.price = price;
+		}
+
+		public String getPicture() {
+			return picture;
+		}
+
+		public void setPicture(String picture) {
+			this.picture = picture;
+		}
+
+		public boolean isPopular() {
+			return popular;
+		}
+
+		public void setPopular(boolean popular) {
+			this.popular = popular;
 		}
 }
